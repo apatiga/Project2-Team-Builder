ef61b7792b24e449ec30ac123342a8e03a445fc8
Pam Nola <pam@cambellcnc.com>
2023-07-17 19:54:35 -0700

    added many files

--- Config/connection.js
+++ Config/connection.js
@@ -1,11 +1,18 @@
 // config/connection.js
 
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 // Create a new Sequelize instance and configure the database connection
-const sequelize = new Sequelize('your-database-name', 'your-username', 'your-password', {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.USER,
+  process.env.DB_PASSWORD,
+ // 'your-database-name', 'your-username', 'your-password',
+ {
   host: 'localhost',
   dialect: 'mysql',
+  port: 3001,
 });
 
 module.exports = sequelize;
