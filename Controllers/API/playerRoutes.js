a08bad0b509df9c10b4c656bda4f2e4cbf59f7bb
apatiga <128877342+apatiga@users.noreply.github.com>
2023-07-19 20:06:30 -0700

    made a signup.handlebars and fixed issue to deply heroku

--- Controllers/API/playerRoutes.js
+++ Controllers/API/playerRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Player } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newPlayer = await Player.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newPlayer);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,19 +17,19 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const playerData = await Player.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!playerData) {
+      res.status(404).json({ message: 'No player found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(playerData);
   } catch (err) {
     res.status(500).json(err);
   }
