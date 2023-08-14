fdb82de809e1536cdc2a9cda1a1c7df1e821ceb3
Pam Nola <pam@cambellcnc.com>
2023-08-14 15:02:14 -0700

    Try for Heroku

--- pblic/js/player.js
+++ pblic/js/player.js
@@ -10,6 +10,21 @@ const gifContainer = document.querySelector(".gif-container");
   // Build an array of all the selected team members
   document.querySelectorAll('.team-member:checked').forEach(item => team.push(item.value));
 
+  //Check for duplicates
+  const players = await debug.players.find({
+    name:{ $in:team },
+    jersey_number: { $in:team }
+  });
+
+  if (players.length > 0) {
+    //There are duplicate players
+    alert('There are duplicate players in your team.  Please remove any duplicates before submitting.');
+  }else{
+    //no duplicate players
+    await debug.teams.insertOne({ team });
+  }
+  }
+
   const response = await fetch('/api/teams', {
     method: 'POST',
     body: JSON.stringify({ team }),
@@ -25,7 +40,7 @@ const gifContainer = document.querySelector(".gif-container");
            
     document.location.reload(true);
     document.location.replace('/team');
-   }, 4000);
+   }, 6000);
     
 
     
@@ -33,7 +48,7 @@ const gifContainer = document.querySelector(".gif-container");
       alert(response.statusText);
   }
 
-};
+
 
 document
   .querySelector('.player-form')
